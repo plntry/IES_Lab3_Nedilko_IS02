6dc5fc84c64c1b7ad64a7f30228d4d4dc193f181
Oksana Nedilko <nedilko7b@gmail.com>
2024-03-15 20:47:39 +0200

    final lab3

---
+++ app/adapters/store_api_adapter.py
@@ -0,0 +1,20 @@
+import json
+import logging
+from typing import List
+
+import pydantic_core
+import requests
+
+from app.entities.processed_agent_data import ProcessedAgentData
+from app.interfaces.store_gateway import StoreGateway
+
+
+class StoreApiAdapter(StoreGateway):
+    def __init__(self, api_base_url):
+        self.api_base_url = api_base_url
+
+    def save_data(self, processed_agent_data_batch: List[ProcessedAgentData]) -> bool:
+        data = [json.loads(item.json()) for item in processed_agent_data_batch]
+
+        response = requests.post(f"{self.api_base_url}/processed_agent_data/", json = data)
+        return response.status_code == requests.codes.ok
